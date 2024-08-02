b0b1d1a90de963dd56abfe29adfcd7617614edb1
AJ ONeal <aj@therootcompany.com>
2024-08-02 13:26:21 -0600

    test: Dash.estimateNthNextProposalCycle()

--- tests/calc-start-end-epoch.js
+++ tests/calc-start-end-epoch.js
@@ -1,40 +1,73 @@
 "use strict";
 
-/** @typedef {Number} Float64 */
-/** @typedef {Number} Uint53 */
-
-let GObj = require("../");
-
-const MS_PER_DAY = 24 * 60 * 60 * 1000;
-
-function logEstimates(estimates) {
-  console.log("Measured Seconds per Block", estimates.secondsPerBlock);
-  console.log("");
-
-  let superblockDeltaDays = estimates.superblockDeltaMs / MS_PER_DAY;
-  console.log(`Until next Superblock (${estimates.superblockHeight}):`);
-  console.log("    Blocks:", estimates.superblockDelta);
-  console.log("    Days:", superblockDeltaDays);
-  console.log("    Date:", estimates.superblockIso);
-  console.log("");
-
-  let voteDeltaDays = estimates.voteDeltaMs / MS_PER_DAY;
-  console.log(`Until next Vote (${estimates.voteHeight}):`);
-  console.log("    Blocks:", estimates.voteDelta);
-  console.log("    Days:", voteDeltaDays);
-  console.log("    Date:", estimates.voteIso);
-  console.log("");
-}
+let DashGov = require("../");
+//let Logger = require("../scripts/estimate.js");
 
 function check2024Aug1() {
-  // let currentBlockHeight = 2114626;
-  // let currentBlockMs = new Date("2024-08-01T22:09:01Z");
   let currentBlockHeight = 2114623;
-  let currentBlockMs = new Date("2024-08-01 22:01:00");
+  let currentBlockTime = "2024-08-01T22:01:00Z";
+  let currentBlockMs = Date.parse(currentBlockTime);
 
-  let estimates = GObj.estimateFutureBlocks(currentBlockMs, currentBlockHeight);
-  // TODO test outputs for rough legitimacy
-  logEstimates(estimates);
+  let cycleCount = 2;
+  let snapshot = {
+    ms: currentBlockMs,
+    block: currentBlockHeight,
+  };
+  let estimates = DashGov.estimateProposalCycles(cycleCount, snapshot);
+
+  if (!estimates.last?.voteDeltaMs) {
+    throw new Error(`missing 'estimates.last'`);
+  }
+  if (estimates.lameduck?.voteDeltaMs) {
+    throw new Error(
+      `has 'estimates.lameduck' during an active proposal period`,
+    );
+  }
+  if (estimates.upcoming?.length !== cycleCount) {
+    throw new Error(
+      `expected '${cycleCount}' upcoming cycles but only got '${estimates.upcoming.length}'`,
+    );
+  }
+
+  //Logger.logEstimates(estimates);
+}
+
+function checkLameduck() {
+  let secondsBefore = 3 * 24 * 60 * 60;
+  let blocksBefore = secondsBefore / DashGov._AVG_SECS_PER_BLOCK;
+  blocksBefore = Math.round(blocksBefore);
+
+  let voteDeadlineHeight = 2108570;
+  let voteDeadlineTime = "2024-07-19T02:56:54.552Z";
+
+  let lameDuckHeight = voteDeadlineHeight - blocksBefore;
+  // '2024-07-22T02:56:54.552Z' - 3 days
+  let lameDuckMs = Date.parse(voteDeadlineTime);
+
+  let cycleCount = 3;
+  let snapshot = {
+    ms: lameDuckMs,
+    block: lameDuckHeight,
+  };
+  let estimates = DashGov.estimateProposalCycles(cycleCount, snapshot);
+
+  if (!estimates.last?.voteDeltaMs) {
+    throw new Error(`missing 'estimates.last'`);
+  }
+  if (!estimates.lameduck?.voteDeltaMs) {
+    throw new Error(`missing 'estimates.lameduck' on lame duck period`);
+  }
+  if (estimates.upcoming?.length !== cycleCount) {
+    throw new Error(
+      `expected '${cycleCount}' upcoming cycles but only got '${estimates.upcoming.length}'`,
+    );
+  }
+
+  //Logger.logEstimates(estimates);
 }
 
 check2024Aug1();
+checkLameduck();
+
+console.info("PASS: DashGov.estimateProposalCycles(count, snapshot)");
+console.info("   - correctly categorizes last, lameduck, and upcoming");
