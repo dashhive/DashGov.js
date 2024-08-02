7e3e53b6dcf31fbd6c8d85a54e0d2f3029d987f7
AJ ONeal <aj@therootcompany.com>
2024-08-02 16:37:58 -0600

    test: DashGov.measureSecondsPerBlock(snapshot)

---
+++ tests/calc-seconds-per-block.js
@@ -0,0 +1,15 @@
+"use strict";
+
+let DashGov = require("../");
+
+let secondsPerBlock = DashGov.estimateSecondsPerBlock();
+if (secondsPerBlock !== DashGov._AVG_SECS_PER_BLOCK) {
+  throw new Error(
+    `expected '${DashGov._AVG_SECS_PER_BLOCK}' got '${secondsPerBlock}', the method of calculating seconds per block has changed (possibly due to using a different block height and timestamp)`,
+  );
+}
+
+console.info(
+  `PASS: DashGov.estimateSecondsPerBlock() == ${DashGov._AVG_SECS_PER_BLOCK}`,
+);
+console.info(`PASS: DashGov.measureSecondsPerBlock(snapshot)`);
