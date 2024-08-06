edd1b4103efeed1b69ca4d34c677db48a9e35c9b
AJ ONeal <aj@therootcompany.com>
2024-08-05 18:08:21 -0600

    test: ./scripts/estimate.js should run without error with provided arguments

---
+++ tests/estimate-script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let Estimate = require("../scripts/estimate.js");
+
+process.argv[2] = "3";
+process.argv[3] = "2106925";
+process.argv[4] = "2024-07-19T02:56:54Z";
+
+Estimate.main();
